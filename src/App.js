234c3b0a3e593ccfb79076aceb239d752b1e927c
Ryan Hiltabrand <ryhiltabrand99@gmail.com>
2022-03-29 14:25:14 -0400

    Simple navbar for later

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import React, { Component } from "react";
+import OurNavbar from "./components/Navbar";
+import ShoppingList from "./components/dmv/test";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Home extends Component {
+  render(){
+    return(
+      <div>
+        <h>Home</h>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    
+
+    return (
+      <div>
+        <OurNavbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/test" element={<ShoppingList />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
